95b59fe830e3bdc146db406b3591b27aa0ce0de1
dengxianxiao <dengxx4@meicloud.com>
2019-04-23 18:20:45 +0800

    添加单词本功能

--- renderer.js
+++ renderer.js
@@ -1,9 +1,11 @@
 const { translateAPI, translateDetailAPI } = require('./src/api/translate')
+const { writeVocabulary } = require('./src/api/vocabulary')
 
 const ZH = 'zh'
 const EN = 'en'
 let translateType = `${EN}-${ZH}`
-let globalWords = '' // 当前要翻译的词
+let currentWord = '' // 当前要翻译的词
+let currentTranslate = [] // 当前翻译的结果
 
 // 绑定翻译类型按钮
 let fromTo = document.getElementById('from-to')
@@ -11,9 +13,9 @@ let toFrom = document.getElementById('to-from')
 fromTo.addEventListener('click', changeTranslateType)
 toFrom.addEventListener('click', changeTranslateType)
 function changeTranslateType(event) {
-  fromTo.className = fromTo.className.replace(/is-active/, '')
-  toFrom.className = toFrom.className.replace(/is-active/, '')
-  event.target.className += ' is-active'
+  fromTo.className = fromTo.className.replace(/is-primary/, '')
+  toFrom.className = toFrom.className.replace(/is-primary/, '')
+  event.target.className += ' is-primary'
   translateType = `${ZH}-${EN}`
   if (event.target.id === 'from-to') {
     translateType = `${EN}-${ZH}`
@@ -21,6 +23,12 @@ function changeTranslateType(event) {
 }
 
 // 绑定翻译按钮
+let inputType = document.getElementById('words')
+inputType.addEventListener('keyup', (e) => {
+  if (e.key === 'Enter') {
+    translateWords()
+  }
+})
 let translateBtn = document.getElementById('translate-button')
 translateBtn.addEventListener('click', translateWords)
 function translateWords() {
@@ -28,18 +36,20 @@ function translateWords() {
   let from = translateTypeArr[0]
   let to = translateTypeArr[1]
   let words = document.getElementById('words').value
-  globalWords = words
+  currentWord = words
 
   if (!from || !to || !words) {
     console.error('翻译参数不能为空')
     alert('翻译参数不能为空')
-    return 
+    return
   }
   translateAPI(from.trim(), to.trim(), words.trim()).then(res => {
     console.log(res)
+    // 处理返回的翻译结果
     let detail = document.getElementById('translate-explain')
     let list = []
     list = res.data.content.word_mean || [res.data.content.out]
+    currentTranslate = list
     let ul = document.getElementById('translate-explain')
     let lis = ul.getElementsByTagName('li')
     // 清空上一次翻译的内容
@@ -62,8 +72,8 @@ function translateWords() {
 
 // 查看更多翻译明细
 function seeMoreDetail() {
-  if (globalWords) {
-    translateDetailAPI(globalWords).then(res => {
+  if (currentWord) {
+    translateDetailAPI(currentWord).then(res => {
       console.log(res)
       let ol = document.getElementById('translate-sentence')
       let lis = ol.getElementsByTagName('li')
@@ -84,5 +94,20 @@ function seeMoreDetail() {
         })
       }
     })
+  }
+}
+
+// 绑定添加单词本按钮
+let addButton = document.getElementById('add-img')
+addButton.addEventListener('click', addVocabulary)
+function addVocabulary() {
+  if (currentWord && currentTranslate) {
+    let obj = {}
+    obj[currentWord] = currentTranslate
+    let a = writeVocabulary(obj)
+    console.log(a)
+    // .then(res => {
+    //   alert('添加成功')
+    // })
   }
 }
---
+++ src/api/vocabulary.js
@@ -0,0 +1,34 @@
+const fs = require('fs')
+
+/**
+ * 写入单词本
+ * @param {Object} word
+ */
+ function writeVocabulary(word) {
+  const vocabularyPath = 'vocabulary.json'
+  return new Promise((resolve, reject) => {
+    fs.readFile(vocabularyPath, 'utf8', (err, data) => {
+      if (err) {
+        // 错误表示文件不存在
+        console.log(err)
+      }
+      if (data) {
+        data = JSON.parse(data)
+        if (Object.keys(data).includes(Object.keys(word)[0])) {
+          resolve(2)
+        }
+      }
+      // 将数据写入文件
+      fs.writeFile(vocabularyPath, JSON.stringify(Object.assign({}, data, word)), err => {
+        if (err) {
+          throw err
+        }
+        resolve(1)
+      })
+    })
+  })
+}
+
+module.exports = {
+  writeVocabulary: writeVocabulary
+}
